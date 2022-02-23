267a16a4b6db531f847ff0bead4965f035b6018f
Kim Hyun Shik <samerj9712@gmail.com>
2022-02-23 12:34:22 +0900

    initial commit

---
+++ GitFlowTest/Protocol.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GitFlowTest
+//
+//  Created by kakao on 2022/02/23.
+//
+
+import Foundation
+
+protocol ItemProtocol {
+    var name: String { get set }
+    var price: Int { get set }
+}
+
+protocol PersonProtocol {
+    var wallet: Int { get set }
+    func buy(item: ItemProtocol)
+}
---
+++ GitFlowTest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  GitFlowTest
+//
+//  Created by kakao on 2022/02/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
